0b75eb03d038e496808a8e54db2240d4bde39df8
宋印赠 <songyinzeng@gmail.com>
2020-07-18 15:00:25 +0800

    用户注册提交

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,9 +1,11 @@
 import request from "../../utils/request";
 
 const login = (param) => request.post("/user/login", param);
+const registe = (param) => request.post("/user/registe", param);
 const list = (param) => request.get("/user/list", param);
 
 export default {
   login,
+  registe,
   list,
 }
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,25 +1,72 @@
 <template>
   <div class="user-login">
-    <el-form class="login-form" ref="form" :model="form" label-width="80px">
-      <div class="login-avatar">
-        <el-avatar
-          :size="100"
-          src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
-        ></el-avatar>
-      </div>
-      <el-form-item label="登录账号">
-        <el-input v-model="form.account"></el-input>
+    <el-form
+      class="login-form"
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRule"
+      label-width="80px"
+      v-show="isLogin"
+    >
+      <el-form-item>
+        <div class="login-avatar">
+          <el-avatar
+            :size="100"
+            src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+          ></el-avatar>
+        </div>
       </el-form-item>
-      <el-form-item label="登录密码">
-        <el-input v-model="form.password" show-password></el-input>
+
+      <el-form-item label="登录账号" prop="account">
+        <el-input v-model="loginForm.account"></el-input>
+      </el-form-item>
+      <el-form-item label="登录密码" prop="password">
+        <el-input v-model="loginForm.password" show-password></el-input>
       </el-form-item>
 
       <el-form-item label="记住我">
-        <el-switch v-model="form.delivery"></el-switch>
+        <el-switch v-model="loginForm.delivery"></el-switch>
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button type="primary" plain @click="isLogin=false">注册</el-button>
+      </el-form-item>
+    </el-form>
+    <el-form
+      class="registe-form"
+      ref="registeForm"
+      :model="registeForm"
+      :rules="registeRule"
+      label-width="80px"
+      v-show="!isLogin"
+    >
+      <el-form-item>
+        <div class="login-avatar">
+          <el-avatar
+            shape="square"
+            :size="100"
+            src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+          ></el-avatar>
+        </div>
+        <el-input style="display:none" v-model="registeForm.password" show-password></el-input>
+      </el-form-item>
+      <el-form-item label="用户名" prop="userName">
+        <el-input v-model="registeForm.userName"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="registeForm.email"></el-input>
+      </el-form-item>
+      <el-form-item label="账号" prop="account">
+        <el-input v-model="registeForm.account"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="registeForm.password" show-password></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button plain @click="isLogin=true">返回登录</el-button>
+        <el-button type="primary" plain @click="registe">提交</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -29,27 +76,84 @@ import userApi from "../../api/user";
 export default {
   data() {
     return {
-      form: {
+      isLogin: true,
+      loginForm: {
         account: null,
         password: null
+      },
+      registeForm: {
+        email: null,
+        userName: null,
+        account: null,
+        password: null
+      },
+      loginRule: {
+        account: [
+          { required: true, message: "请输入登录账号", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" }
+        ]
+      },
+      registeRule: {
+        account: [
+          { required: true, message: "请输入登录账号", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" }
+        ],
+        email: [{ required: true, message: "请输入邮箱", trigger: "blur" }],
+        userName: [{ required: true, message: "请输入用户名", trigger: "blur" }]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
-      userApi.login(this.form).then(user => {
-        sessionStorage.setItem("user", JSON.stringify(user));
-        sessionStorage.setItem("token", user.token);
-
-        this.$router.push({ path: "/message/inBox" });
+    validateForm(form) {
+      return new Promise((resolve, reject) => {
+        this.$refs[form].validate(valid => {
+          if (valid) {
+            resolve("表单校验通过");
+          } else {
+            reject("表单校验未通过");
+          }
+        });
+      }).catch(err => {
+        this.$notify({
+          title: "警告",
+          message: "请确认信息合法性",
+          type: "warning"
+        });
+        throw new Error(err);
       });
+    },
+    login() {
+      this.validateForm("loginForm")
+        .then(() => userApi.login(this.loginForm))
+        .then(user => {
+          sessionStorage.setItem("user", JSON.stringify(user));
+          sessionStorage.setItem("token", user.token);
+
+          this.$router.push({ path: "/message/inBox" });
+        });
+    },
+    registe() {
+      this.validateForm("registeForm")
+        .then(() => userApi.registe(this.registeForm))
+        .then(user => {
+          this.isLogin = true;
+          this.$notify({
+            title: "提示",
+            message: "激活链接已发送至邮箱，请尽快前往激活",
+            type: "success"
+          });
+        });
     }
   }
 };
 </script>
 <style scoped>
-.login-form {
+.login-form,
+.registe-form {
   position: absolute;
   top: 20%;
   left: 25%;
